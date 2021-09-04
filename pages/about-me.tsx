ad1fcd911ea525fd97103351a3f3303b01609a0c
xinninggong <xinning.gong@gmail.com>
2021-09-04 22:16:16 +1200

    Add about me

--- pages/about-me.tsx
+++ pages/about-me.tsx
@@ -26,15 +26,16 @@ export default function AboutMe() {
                   className={utilStyles.rightSpacedIcon}
                   color="CornflowerBlue"
                 />
-                Development Experience
+                Data Experience
               </Card.Title>
               <Card.Text className={utilStyles.cardText}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur.
+                Data is an ever growing epidemic with the potential of infinite knowledge. 
+                I have learnt to wrangle and analyse data using R. 
+                I especially enjoyed data mining and collecting data from websites to dissect and find patterns and trends. 
+                I also enjoyed using ggplot to present my findings through data visualisation. 
+                I have worked with databases using SQLite, DB browser, MySQL, Microsoft SQL Server and data modelling through ERD. 
+                I am currently learning about amazon cloud services and the endless data amazon is able to store. 
+                
               </Card.Text>
             </Card.Body>
           </Card>
@@ -46,15 +47,13 @@ export default function AboutMe() {
                   className={utilStyles.rightSpacedIcon}
                   color="LightGreen"
                 />
-                Testing Experience
+                Alpha Tester
               </Card.Title>
               <Card.Text className={utilStyles.cardText}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur.
+                Do you ever just start thinking about the craziest ides while taking a shower? 
+                One day, while I was showering, I started thinking about creating an app to match students with peers of similar interests and studies using an algorithm based on their personality. 
+                I quickly forgot and went on with my life until I saw on reddit that a group of UoA students had actually developed the app. 
+                I became very invested in the app and offered to become an early adopter and alpha tester. You may even see some of my ideas becoming features on the app Fistbump. 
               </Card.Text>
             </Card.Body>
           </Card>
@@ -66,15 +65,12 @@ export default function AboutMe() {
                   className={utilStyles.rightSpacedIcon}
                   color="Coral"
                 />
-                Student Experience
+                Education
               </Card.Title>
               <Card.Text className={utilStyles.cardText}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur.
+              To aid my journey in conquering the world using data, I am currently a penultimate Bachelor of Science student at the University of Auckland. 
+          I chose to study a double major in IT Management and Statistics to explore both the technical and practical sides of data in commerce and science. 
+          This combination gave me more flexibility to learn a myriad of papers such as computer science, business analytics, statistics, information systems and operations management. 
               </Card.Text>
             </Card.Body>
           </Card>
@@ -86,15 +82,14 @@ export default function AboutMe() {
                   className={utilStyles.rightSpacedIcon}
                   color="Plum"
                 />
-                Other Experience
+                Business
               </Card.Title>
               <Card.Text className={utilStyles.cardText}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur.
+                Everything in life is a process and business is simply a system of processes. 
+                Through information systems and business analysis, I was able to learn how to apply the knowledge and information from data to produce business value. 
+                I learnt about project initiation and management, how to gather and prioritise requirements and also the agile methodology. 
+                I really enjoyed working with my group to produce a project proposal and final deliverable for a job search web portal. 
+                We all came from different backgrounds and brought different strengths to the group. 
               </Card.Text>
             </Card.Body>
           </Card>
@@ -106,15 +101,14 @@ export default function AboutMe() {
                   className={utilStyles.rightSpacedIcon}
                   color="PaleGoldenRod"
                 />
-                Another Experience
+                Coding Journey
               </Card.Title>
               <Card.Text className={utilStyles.cardText}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur.
+                Despite growing up in the small city of Palmerston North, I had the privilege of studying Computer Science since high school. 
+                I have learnt python and front end web development for 5 years now. 
+                At university, I am currently learning java programming to develop software applications through object oriented design principles.
+                Through attending hackathons and workshops, I found that I was able to pick up coding quickly. Here, I was able to experiment with chrome extensions and typescript for React. 
+       
               </Card.Text>
             </Card.Body>
           </Card>
@@ -126,15 +120,15 @@ export default function AboutMe() {
                   className={utilStyles.rightSpacedIcon}
                   color="LightBlue"
                 />
-                More Experience
+                Continuous improvement
               </Card.Title>
               <Card.Text className={utilStyles.cardText}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur.
+                "Continuous improvement is better than delayed perfection". 
+                In operations management, I learnt about the lean management and decided to adopt this philosophy in my everyday life. 
+                To keep building and improving myself, I have been actively attending hackathons and workshops. 
+                I also joined the Summer of Tech program and am currently a mentee at Cisco.
+                Through Cisco, I was able to attend many personal development workshops such as building your personal brand, identifying strengths and leadership. 
+                
               </Card.Text>
             </Card.Body>
           </Card>
@@ -146,15 +140,14 @@ export default function AboutMe() {
                   className={utilStyles.rightSpacedIcon}
                   color="BurlyWood"
                 />
-                Other Interests
+                Me, myself and I
               </Card.Title>
               <Card.Text className={utilStyles.cardText}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur.
+                This year my goal was to try something new and really put myself out there.
+                Hence, I decided to join the Auckland Student Pool Association. 
+                It was interesting to meet different people and learn about their backgrounds while playing pool. 
+                I have to admit that while my pool skills have been gradually improving, luck is usually what helps me to win. 
+
               </Card.Text>
             </Card.Body>
           </Card>
--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMdCentre}>
-        <p className="mb-2 font-weight-bold">Hardworking Student</p>
+        <p className="mb-2 font-style-italic">"Curiousity is the fuel for discovery, inquiry and learning."</p>
 
         <div className="mb-2">
           <Link href="/about-me">
@@ -33,13 +33,31 @@ export default function Home() {
         <SocialLinks />
 
         <p className={utilStyles.openingPara}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          My curiousity drives my energy for all things data. The constant learning and journey through data fascinates me. 
+          There is infinite discovery, insights and innovation waiting to be found. 
+          That being said, data science is not always a walk in the park. It will never just fall in your lap and work out perfectly. 
+          It is not like accounting where eventually everything should eventually balance. 
+          It is not like law where there's ultimately only one outcome. 
+          But I enjoy a good challenge and every data is different. 
+          The challenging elements of data science offers opportunity to continuously improve and expand skills and knowledge which I believe is important. 
+        </p> 
+        <br></br>
+        <p className={utilStyles.openingPara}>
+          To aid my journey in conquering the world using data, I am currently a penultimate Bachelor of Science student at the University of Auckland. 
+          I chose to study a double major in IT Management and Statistics to explore both the technical and practical sides of data in commerce and science. 
+          This combination gave me more flexibility to learn a myriad of papers such as computer science, business analytics, statistics, information systems and operations management. 
+          
+        </p>
+        <br></br>
+        <p className={utilStyles.openingPara}>
+          I was able to further develop my skills with python and front end web development that I had already learn for 3 years in high school. 
+          I also learnt new skills such as using R to wrangle and analyse data, java programming to develop software applications through object oriented design principles and data modelling like SQL and ERD models. 
+          To challenge myself and my skills, I have participated in hackathons and workshops. I also joined the Summer of Tech program and am currently a mentee at Cisco. You can view some of my projects through the work showcase button above. 
+
+        </p>
+        <br></br>
+        <p className={utilStyles.openingPara}>
+          That's enough about me for now. Yours Truly, Jennie. 
         </p>
       </section>
     </Layout>
