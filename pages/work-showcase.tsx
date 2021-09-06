9f0b87c7d015ecc98f49591aed5dd0526146d60f
xinninggong <xinning.gong@gmail.com>
2021-09-06 14:57:10 +1200

    Update info

--- pages/work-showcase.tsx
+++ pages/work-showcase.tsx
@@ -17,7 +17,7 @@ export default function WorkShowcase() {
       </Head>
       <section className={utilStyles.headingMdCentre}>
         <SocialLinks />
-        <h1 className="mb-4 mt-3 font-weight-bold">My Work</h1>
+        <h1 className="mb-4 mt-3 font-weight-bold">My Projects</h1>
         <Container>
           <Row>
             <Col md>
@@ -48,22 +48,26 @@ export default function WorkShowcase() {
             </Col>
             <Col md>
               <Card className="bg-light text-left mb-4">
-                <Card.Img variant="top" src="/images/template.jpg" />
+                <Card.Img variant="top" src="/images/stats330.png" />
                 <Card.Body>
-                  <Card.Title>SoT Dev Demo</Card.Title>
+                  <Card.Title>Statistical Modelling</Card.Title>
                   <Card.Text className={utilStyles.cardText}>
-                    This is an SoT dev demo project.
+                    Last semester, I completed a statistical modelling paper aimed to broaden understanding of generalised linear modles and related methods. 
+                    My research was on the effect of insulin on laboratory mice and predicting oxygen consumption in a person. 
+                    I used simulation-based procedures, such as bootstrapping and cross-validation, 
+                    to provide robust inference, investigate consequences of assumption violations, and to solve goodness-of-fit and model-selection problems.
+
                   </Card.Text>
                   <Button
                     variant="outline-dark"
                     className="mb-1"
-                    href="https://trademe.co.nz"
+                    href="https://github.com/xinninggong/Statistical-Computing"
                   >
                     <FontAwesomeIcon
                       icon={["fab", "github"]}
                       className={utilStyles.rightSpacedIcon}
                     />
-                    Trade Me
+                    View Report
                   </Button>
                 </Card.Body>
               </Card>
@@ -72,44 +76,53 @@ export default function WorkShowcase() {
             <Row>
             <Col md>
               <Card className="bg-light text-left mb-4">
-                <Card.Img variant="top" src="/images/template.jpg" />
+                <Card.Img variant="top" src="/images/job.png" />
                 <Card.Body>
-                  <Card.Title>SoT Dev Demo</Card.Title>
+                  <Card.Title>Career Web Portal</Card.Title>
                   <Card.Text className={utilStyles.cardText}>
-                    This is an SoT dev demo project. Hi everyone.
+                    This project was to create a job protal prototype.
+                    We had to first write a proposal to seek approval by the project manager consisting of the identified problems, use case model, functions outline and visual preview. 
+                    The second part was designing the mobile and desktop GUI of the main use cases. We focused on the Microsoft user centred design principles. 
+                    The final deliverable consists of system architecture, prototype documentation with functionalities, data outline, demonstration and test plan. 
+
                   </Card.Text>
                   <Button
                     variant="outline-dark"
                     className="mb-1"
-                    href="https://trademe.co.nz"
+                    href="https://github.com/xinninggong/Job-search-web-portal"
                   >
                     <FontAwesomeIcon
                       icon={["fab", "github"]}
                       className={utilStyles.rightSpacedIcon}
                     />
-                    Aorthi is awesome
+                    Career Portal 
                   </Button>
                 </Card.Body>
               </Card>
             </Col>
             <Col md>
               <Card className="bg-light text-left mb-4">
-                <Card.Img variant="top" src="/images/template.jpg" />
+                <Card.Img variant="top" src="/images/bike.png" />
                 <Card.Body>
-                  <Card.Title>SoT Dev Demo</Card.Title>
+                  <Card.Title>Citi Bikes Analysis</Card.Title>
                   <Card.Text className={utilStyles.cardText}>
-                    This is an SoT dev demo project. Hi everyone.
+                    Citibike is an electric bike rental company in New York City. 
+                    My research was based on the 2018 Citi Bike trip data in New York City. 
+                    I performed web scraping to retrieve information from their website. 
+                    I polished and tidied the data to be ready for analysis and produced visualisations using ggplot2. 
+
+
                   </Card.Text>
                   <Button
                     variant="outline-dark"
                     className="mb-1"
-                    href="https://trademe.co.nz"
+                    href="https://github.com/xinninggong/Data-Technologies"
                   >
                     <FontAwesomeIcon
                       icon={["fab", "github"]}
                       className={utilStyles.rightSpacedIcon}
                     />
-                    Aorthi is awesome
+                    Citi Bikes Research
                   </Button>
                 </Card.Body>
               </Card>
